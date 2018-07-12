34dc6c91b0ff6717ab2feb98461df531040db391
Bioshaun <ricekent@163.com>
2018-07-12 10:00:32 -0400

    accelerate it by replace for loop

--- bioinformatics/analysis/expression/co_expression/co_exp_mRNA.py
+++ bioinformatics/analysis/expression/co_expression/co_exp_mRNA.py
@@ -1,52 +1,58 @@
 import pandas as pd
 import click
 
+
 @click.command()
 @click.option(
-    '-t',
-    '--ts_score',
-    help='tissue specific score table.',
+    '-g',
+    '--gene_module',
+    help='gene module table.',
     type=click.Path(dir_okay=False),
     required=True
 )
 @click.option(
     '-c',
     '--cor_table',
-    help='mRNA lncRNA correlation table.',
+    help='correlation table.',
     type=click.Path(dir_okay=False),
     required=True
 )
 @click.option(
     '-o',
     '--output',
-    help='tissue correlated mRNA results.',
+    help='module correlated mRNA results.',
     type=click.Path(dir_okay=False),
     required=True
 )
-def main(ts_score, cor_table, output):
-    ts_score_df = pd.read_table(ts_score, index_col=0)
-    cor_df = pd.read_table(cor_table, header=None)
-    cor_df.columns = ['lncRNA', 'mRNA', 'cor', 'pvalue', 'padj']
-    mrnas = cor_df.mRNA.unique()
-    cor_df = cor_df.set_index(['mRNA', 'lncRNA'])
-    tissues = ts_score_df.tissue.unique()
-    mrna_cor_dict = {}
-    for each_ts in tissues:
-        each_ts_lnc = ts_score_df[ts_score_df.cluster_tissue == each_ts].index
-        each_non_ts_lnc = ts_score_df[ts_score_df.cluster_tissue != each_ts].index
-        for each_mrna in mrnas:
-            each_ts_cor = cor_df.loc[each_mrna].loc[each_ts_lnc].cor.mean()
-            each_non_ts_cor = cor_df.loc[each_mrna].loc[each_non_ts_lnc].cor.mean()
-            mrna_cor_dict.setdefault('mRNA', []).append(each_mrna)
-            mrna_cor_dict.setdefault('tissue', []).append(each_ts)
-            mrna_cor_dict.setdefault('tissue_cor', []).append(each_ts_cor)
-            mrna_cor_dict.setdefault('non_tissue_cor', []).append(each_non_ts_cor)
-            mrna_cor_dict.setdefault('diff', []).append(each_ts_cor - each_non_ts_cor)
-    mrna_cor_df = pd.DataFrame(mrna_cor_dict)
-    mrna_cor_out_df = mrna_cor_df.loc[:, ['mRNA', 'tissue', 'tissue_cor', 'non_tissue_cor', 'diff']]
-    mrna_cor_out_df.to_csv(output, sep='\t', index=False)
+@click.option(
+    '-n',
+    '--col_num',
+    help='target gene column number',
+    default=2,
+    type=click.INT
+)
+def main(gene_module, cor_table, output, col_num):
+    index_col = col_num - 1
+    gm_df = pd.read_table(gene_module)
+    cor_df = pd.read_table(cor_table, header=None,
+                           index_col=index_col,
+                           names=['mRNA', 'cor', 'pvalue', 'padj'])
+    cor_gm_df = pd.merge(cor_df, gm_df,
+                         left_index=True, right_on='gene_id')
+    cor_gm_mean = cor_gm_df.groupby(['mRNA', 'module_name'])['cor'].mean()
+    cor_gm_sum = cor_gm_df.groupby(['mRNA', 'module_name'])['cor'].sum()
+    cor_total = cor_gm_df.groupby(['mRNA'])['cor'].sum()
+    cor_gm_size = cor_gm_df.groupby(['mRNA', 'module_name'])['cor'].size()
+    cor_total_size = cor_gm_df.groupby(['mRNA']).size()
+    cor_gm_other = cor_total - cor_gm_sum
+    cor_gm_other_size = cor_total_size - cor_gm_size
+    cor_gm_other_cor = cor_gm_other / cor_gm_other_size
+    cor_gm_diff = cor_gm_mean - cor_gm_other_cor
+    cor_gm_summary = pd.concat(
+        [cor_gm_mean, cor_gm_other_cor, cor_gm_diff], axis=1)
+    cor_gm_summary.columns = ['module_cor', 'non_module_cor', 'diff']
+    cor_gm_summary.to_csv(output, sep='\t')
 
 
 if __name__ == '__main__':
     main()
-
